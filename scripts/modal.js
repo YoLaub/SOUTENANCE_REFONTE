eb07398440cf05c2dd2fbb6b7a631d5bc889052a
YoLaub <johngreta904@gmail.com>
2024-11-25 16:07:02 +0100

    correctif résponsive

--- scripts/modal.js
+++ scripts/modal.js
@@ -3,14 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const modal = document.getElementById('modal');
     const modalDescription = document.getElementById('modal-description');
     const modalPrice = document.getElementById('modal-price');
+    const modalDetail = document.getElementById('modal-details')
     const closeModal = document.getElementById('close-modal');
 
     cards.forEach(card => {
         card.addEventListener('click', function () {
             const description = card.getAttribute('data-description');
             const price = card.getAttribute('data-price');
+            const details = card.getAttribute('data-details')
             modalDescription.textContent = description;
-            modalPrice.textContent = 'Price: $' + price;
+            modalPrice.textContent = 'Tarif: $' + price;
+            modalDetail.textContent = "-" + details;
             modal.style.display = 'block';
         });
     });
