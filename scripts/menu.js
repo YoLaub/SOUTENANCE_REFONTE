e7ab7c380552ea2a5ec6fd62e45aa0ac7b3ca654
YoLaub <johngreta904@gmail.com>
2024-11-27 16:17:24 +0100

    Page contact mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,23 +1,28 @@
+const screen = window.matchMedia("(max-width: 320px");
+
 document.getElementById("menu-icon").addEventListener("click", function () {
     var menu = document.getElementById("menuBurger");
-    var currentTop = parseFloat(menu.style.top) || -700; // Convertir en nombre ou utiliser une valeur par défaut
+    var currentTop = parseFloat(menu.style.top); // Convertir en nombre 
 
-    if (currentTop >= -700) {
-        menu.style.top = '-346px';
-        menu.style.transition = "top 0.6s ease";
-    } else {
-        menu.style.top = "-732px";
-        menu.style.transition = "top 0.6s ease";
-    }
-
-    document.getElementById("cross").addEventListener("click", function () {
-        var currentTop = parseFloat(menu.style.top) || -258; // Convertir en nombre ou utiliser une valeur par défaut
-    
-        if (currentTop >= -346) {
-            menu.style.top = "-732px";
+    if (screen.matches) {
+        if (currentTop <= -700) {
+            menu.style.top = '-377px';
+            menu.style.transition = "top 0.6s ease";
+        } else {
+            menu.style.top = "-797px";
             menu.style.transition = "top 0.6s ease";
         }
-    });
-});
+    } else {
+        if (currentTop <= -700) {
+            menu.style.top = '-346px';
+            menu.style.transition = "top 0.6s ease";
+        } else {
+            menu.style.top = "-738px";
+            menu.style.transition = "top 0.6s ease";
+        }
+    }
+}
+
+);
 
 
