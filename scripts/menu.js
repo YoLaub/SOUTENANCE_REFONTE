a127809456cad2ad987bc329e4e94156f0fcc579
YoLaub <johngreta904@gmail.com>
2024-11-27 02:19:49 +0100

    test

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,19 +1,23 @@
-document.getElementById("menu-icon").addEventListener("click", function(){
+document.getElementById("menu-icon").addEventListener("click", function () {
     var menu = document.getElementById("menuBurger");
-    if (menu.style.left === "-413px"){
-        menu.style.left = '0px'
-        menu.style.transition= " left 0.6s ease"
-    }else{
-        menu.style.left="-413px"
+    var currentTop = parseFloat(menu.style.top) || -700; // Convertir en nombre ou utiliser une valeur par défaut
+
+    if (currentTop >= -700) {
+        menu.style.top = '-346px';
+        menu.style.transition = "top 0.6s ease";
+    } else {
+        menu.style.top = "-732px";
+        menu.style.transition = "top 0.6s ease";
     }
 
+    document.getElementById("cross").addEventListener("click", function () {
+        var currentTop = parseFloat(menu.style.top) || -258; // Convertir en nombre ou utiliser une valeur par défaut
+    
+        if (currentTop >= -346) {
+            menu.style.top = "-732px";
+            menu.style.transition = "top 0.6s ease";
+        }
+    });
+});
 
 
-document.getElementById("cross").addEventListener("click", function(){
-    if (menu.style.left === "0px"){
-        menu.style.left = "-413px";
-        menu.style.transition = " right 0.6s ease";
-    }
-})
-
-})
