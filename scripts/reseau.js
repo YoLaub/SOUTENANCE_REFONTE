8544f23cb5b39cb5fc67ed3e45aa54d180d81d38
YoLaub <johngreta904@gmail.com>
2024-12-16 09:56:14 +0100

    amélioration animation

--- scripts/reseau.js
+++ scripts/reseau.js
@@ -1,16 +1,24 @@
 document.getElementById("shareDesktop").addEventListener("click", function () {
 
     var shareNet = document.getElementById("network");
+    var blockShare = document.getElementById("shareDesktop");
 
         if (shareNet.style.display === "none") {
-            shareNet.style.display = "flex";
+            blockShare.style.height="172px";
+            setTimeout(() => {
+                shareNet.style.display = "flex";
+            }, 400);
+            setTimeout(() => {
+                shareNet.style.scale = "1";
+            }, 450);
+            
+            
         } else {
             shareNet.style.display = "none";
+            blockShare.style.height="60px";
+            shareNet.style.scale = "0";
         }
 });
-
-
-
 
 
 
