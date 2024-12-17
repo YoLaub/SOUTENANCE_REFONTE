3ad46c7be55b6921ea3b370a246645f1c65cbf13
YoLaub <johngreta904@gmail.com>
2024-12-17 16:56:34 +0100

    minification

---
+++ scripts/menuTwo-min.js
@@ -0,0 +1,2 @@
+document.getElementById("hookMenu").addEventListener("click",function(){var miniMenu=document.getElementById("menuTwo");var blockMenu=document.getElementById("hookMenu");if(miniMenu.style.display==="none"){miniMenu.style.display="flex";blockMenu.style.maxWidth="240px";setTimeout(()=>{miniMenu.style.opacity="1";},40);}else{miniMenu.style.display="none"
+blockMenu.style.maxWidth="150px";miniMenu.style.opacity="0";}});
